c4acb1608152dbcfc5a770d868dcd6698ab589f8
Cordes-RJ <richardj.cordes@gmail.com>
2019-06-21 10:12:17 -0400

    Added a C example, and noted hanging functions

--- code/intro/intro_02_GoRoutineMemory.go
+++ code/intro/intro_02_GoRoutineMemory.go
@@ -19,9 +19,17 @@ func intro02memoryA() {
 }
 
 // We'd expect an output of a:1, b:2 ...
-// but instead, we get d:3 d:3 d:3...
-// You may also get an error on the line, sugges
+// but instead, I get d:3 d:3 d:3...
+// You may also get an error on the line, suggesting
+// a loop variable is captured by the func literal-
+// because the Goroutines can run at ANY point in the
+// future, which values will print from the goroutine
+// are undetermined
 
+// if we instead pass in the objects as shown below
+// the function scope will create a deep copy of the
+// items within the new scope, giving us predictable
+// outcomes
 func intro02memoryB() {
 	var wg sync.WaitGroup // this is a WaitGroup
 	for i, item := range []string{"a", "b", "c", "d"} {
@@ -33,3 +41,37 @@ func intro02memoryB() {
 	}
 	wg.Wait()
 }
+
+// go routines are given a few kb to start, if it's not
+// enough, runtime will grow or reduce it as necessary.
+// This allows many goroutines to exist, and given
+// that it takes only 3-5 inexpensive instructions per
+// function call, it is fairly inexpensive and practical
+// to instantiate hundreds of thousands of goroutines
+// without straining system resources.
+func intro02memoryC() {
+	var wg sync.WaitGroup
+	// change this   v number to see what happens
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			fmt.Printf("[%d]", i)
+		}(i)
+	}
+	wg.Wait()
+}
+
+// However note that the garbage collector does not have
+// the responsibility of closing routines.
+// if a routine has been abandoned, it will exist until
+// the process exits. This is referred to as a "hang"
+/*
+func hangfunc() {
+	go func() {
+		for{
+			// hangs forever
+		}
+	}()
+}
+*/
