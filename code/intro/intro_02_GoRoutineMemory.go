d4340348c707c5d6ebc02d5918b39c461b8e1adb
Cordes-RJ <richardj.cordes@gmail.com>
2019-06-21 10:00:23 -0400

    Added A and B examples for intro_02

--- code/intro/intro_02_GoRoutineMemory.go
+++ code/intro/intro_02_GoRoutineMemory.go
@@ -1,14 +1,35 @@
 package intro
 
-import "sync"
-import util "GoPatterns/code/util"
+import (
+	"fmt"
+	"sync"
+)
 
 // If we look at the function below:
 func intro02memoryA() {
 	var wg sync.WaitGroup // this is a WaitGroup
 	for i, item := range []string{"a", "b", "c", "d"} {
 		wg.Add(1)
-		go util.MakeAThing(item, i).DoWithWG(&wg)
+		go func() {
+			defer wg.Done()
+			fmt.Printf("| $%s : %d |", item, i)
+		}()
+	}
+	wg.Wait()
+}
+
+// We'd expect an output of a:1, b:2 ...
+// but instead, we get d:3 d:3 d:3...
+// You may also get an error on the line, sugges
+
+func intro02memoryB() {
+	var wg sync.WaitGroup // this is a WaitGroup
+	for i, item := range []string{"a", "b", "c", "d"} {
+		wg.Add(1)
+		go func(item string, i int) {
+			defer wg.Done()
+			fmt.Printf("| $%s : %d |", item, i)
+		}(item, i)
 	}
 	wg.Wait()
 }
