4268aa32d6506a331dcb214446f3d485fc089c31
Cordes-RJ <richardj.cordes@gmail.com>
2019-07-09 11:34:48 -0400

    Added Explanation

--- code/introChannels/intro_08_RangeOverChannel.go
+++ code/introChannels/intro_08_RangeOverChannel.go
@@ -2,6 +2,8 @@ package introChannels
 
 import "fmt"
 
+// RangeOverChan08 shows how one might use a channel's closure as a means of
+// signaling between routines
 func RangeOverChan08() {
 	intPipe := make(chan int) // 1
 	go func() {
@@ -14,3 +16,9 @@ func RangeOverChan08() {
 		fmt.Printf("[%d]", v)
 	}
 }
+
+// [1] Make Channel
+// [2] push to pipe until index passes 5
+// [3] This range function will wait for values to pass through intpipe until
+// intpipe is closed, effectively creating a wait() function without use of a
+// WaitGroup
